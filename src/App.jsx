3695abcf2d95fcb87858f36d241505ef219bb5ba
Samikshya Paudel <samikshyapaudel61@gmail.com>
2025-06-09 18:28:55 +0545

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,29 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './assets/public/Login';
 import Register from './assets/public/Register';
 import Home from './assets/public/Home';
+import Register from '../public/Register';
  
 function App() { 
-  return ( 
+  // return ( 
     
-    <Routes> 
-      <Route path="/" element={<Home />} /> 
-      <Route path="/login" element={<Login />} /> 
-      <Route path="/register" element={<Register />} /> 
-    </Routes> 
+  //   // <Routes> 
+  //   //   <Route path="/" element={<Home />} /> 
+  //   //   <Route path="/login" element={<Login />} /> 
+  //   //   <Route path="/register" element={<Register />} /> 
+  //   // </Routes> 
 
-    
+
+     
+
+  // ); 
+  return(
+  
     <div> 
       <h1>React Hook Form Example</h1> 
       <Register /> 
-    </div> 
+    </div>
 
-  ); 
+  );
 } 
  
 export default App;
